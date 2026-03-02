57afc4b81d2e6f0e9b7931ebea2182f80630719f
AskaronDSS <dssergey94@gmail.com>
2026-03-02 16:13:15 +0200

    translate

--- Django/main/forms.py
+++ Django/main/forms.py
@@ -5,11 +5,19 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
+    email = forms.EmailField(label='Электронная почта')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].label = "Пароль"
+        self.fields['password2'].label = "Повторите пароль"
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        labels = {
+            'username': 'Имя пользователя'
+        }
 
 
 class PredictionForm(forms.Form):
